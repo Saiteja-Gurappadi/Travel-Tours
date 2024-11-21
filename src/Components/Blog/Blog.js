d0f8ca17a580859350daf381ffa546bbd47165c5
Saiteja-Gurappadi <saitejagvs8@gmail.com>
2024-11-21 23:02:18 +0530

    fourth commit

--- src/Components/Blog/Blog.js
+++ src/Components/Blog/Blog.js
@@ -1,24 +1,10 @@
-import React, { useState } from 'react'
-import './BlogStyle.css'
+import React from 'react'
 const Blog = () => {
-    const [change,setChange] = useState("Sai");
-    const [count,setCount] = useState(0);
-    const handleRename = ()=>{
-        setChange("Snake")
-        
-        if (count>=10 && count <30){
-            setCount(count+10)
-        }
-    
-        else{
-            setCount(count+1)
 
-        }
-    }
   return (
-    <div>
-        <h1>hi my name is {change} {count}</h1>
-        <button onClick={handleRename}>{change}</button>
+    <div className='text-center'>
+        <h1 className='text-xl bg-yellow-100 text-white'>Companys Blog</h1>
+
     </div>
   )
 }
